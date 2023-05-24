17c29da2e6cc3a9f646f7ed8ba1545a0409ec9bd
sliaprjs <sliaprjs@gmail.com>
2023-05-24 11:52:47 +0300

    add requests to notes

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
+import noteService from './services/notes';
 import Note from "./components/Note"
 
 const App = () => {
@@ -9,10 +10,7 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/notes').then(response => {
-      console.log('Fullfilled');
-      setNotes(response.data);
-    })
+    noteService.getAll().then(initialNotes => setNotes(initialNotes))
   }, [])
 
   const addNote = (e) => {
@@ -24,12 +22,29 @@ const App = () => {
       id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObj));
-    setNewNote('');
+    noteService.create(noteObj).then(returnedNote => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote('');
+    })
   }
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value);
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id);
+    const changedNote = {...note, important: !note.important}
+
+    noteService.update(id, changedNote).then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    }).catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
@@ -43,7 +58,7 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note}/>)}
+        {notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>)}
       </ul>
       <form onSubmit={addNote}>
         <input type="text" value={newNote} onChange={handleNoteChange}/>
